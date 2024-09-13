3ed505c50932fc15839ebdbc837869e50ad52253
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-09-13 17:45:19 +0300

    clone museum layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const iconMenu = document.querySelector('.icon-menu');
+
+if (iconMenu) {
+  iconMenu.addEventListener('click', function(e) {
+    document.documentElement.classList.toggle('menu-open');
+  });
+}
+
+document.addEventListener('click', (e) => {
+  if (
+    e.target !== iconMenu
+    && document.documentElement.classList.contains('menu-open')
+  ) {
+    document.documentElement.classList.remove('menu-open');
+  }
+});
